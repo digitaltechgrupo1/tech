f6b2ce9aaae90bd9d10f4af12385acaca6694667
Sergio Almeciga <sergioalme24@hotmail.com>
2022-01-03 22:26:42 -0500

    se modifica codigo

--- holagrupo.js
+++ holagrupo.js
@@ -1,1 +1,10 @@
-console.log("Hola grupo1")
+function prueba (n){
+    if (n>2){
+        console.log("correcto")
+    }
+    else {
+        console.log("incorrecto")
+    }
+
+}
+console.log(prueba(3))
